4dc050fac2799f4e67cb98cc27a6e688310202ea
darkxday <darkxday@gmail.com>
2022-04-05 15:11:17 +0800

    poop

---
+++ Complete Beginner/Module 3/initialisingvariables.py
@@ -0,0 +1,10 @@
+#while my_variable < 10:
+#    print("Hello")
+#    my_variable +=1
+
+# my_variable is not defined and so will error
+
+my_variable = 1
+while my_variable < 10:
+    print("Hello")
+    my_variable +=1
